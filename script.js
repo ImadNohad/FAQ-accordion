edd47c2c0626ae3acc1f96cee339d5af10423244
Imad Nohad <imadnohad@gmail.com>
2025-03-14 10:12:13 +0000

    first

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-function toggleAccordionItem(item){
-    item.classList.toggle("open")
-}
-
-let plusIcons = document.querySelectorAll(".plus-sign")
-for(let plus of plusIcons){
-    plus.addEventListener("click", function(){
-        let itemBody = this.parentElement.nextElementSibling
-        toggleAccordionItem(itemBody)
-        if(itemBody.classList.contains("open"))
-            this.src = "./assets/images/icon-minus.svg"
-        else
-            this.src = "./assets/images/icon-plus.svg"
+//Méthode sans onclick dans le code html de l'image
+let titles = document.querySelectorAll(".accordion-item-title")
+for(let title of titles){
+    title.addEventListener("click", function(){
+        let itemBody = this.nextElementSibling
+        let img = title.querySelector("img")
+        itemBody.classList.toggle("open")
+        img.src = itemBody.classList.contains("open") ? "./assets/images/icon-minus.svg" : "./assets/images/icon-plus.svg"
     })
 }
+
+//Méthode avec onclick dans le code html de l'image
+//décommenter si vous voulez utiliser onclick
+// function openBody(title) {
+//     let itemBody = title.nextElementSibling
+//     itemBody.classList.toggle("open")
+//     let img = title.querySelector("img")
+//     img.src = itemBody.classList.contains("open") ? "./assets/images/icon-minus.svg" : "./assets/images/icon-plus.svg"
+// }
